4621ae11a6725394458e187b46304b0dc2769eae
rpsohag <rpsohag.bd@gmail.com>
2025-03-24 19:34:22 +0600

    implement authentication using clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import {ClerkProvider} from '@clerk/nextjs'
 
 const inter = Inter({
   variable: "--font-inter",
@@ -20,12 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        {children}
-        </ThemeProvider>
-      </body>
-    </html>
+        <ClerkProvider>
+          <html lang="en" suppressHydrationWarning>
+            <body className={inter.className}>
+              <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+              {children}
+              </ThemeProvider>
+            </body>
+          </html>
+        </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { ModeToggle } from "@/components/shared/theme-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex justify-between items-center p-10">
       <h1 className="font-inter">ShopNow</h1>
+      <div>
+        <UserButton/>
       <ModeToggle />
+      </div>
     </div>
   );
 }
